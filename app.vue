05559e1708d76708ef689cc7093e874073a3a2e5
Norton Almeida Junior <norton.junior@defensoria.mg.def.br>
2024-03-15 16:23:57 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    'nuxt-primevue'
+  ],
+  primevue: {
+    theme: 'saga-blue',
+    ripple: true,
+    cssLayerOrder: 'reset,primevue',
+    directives: {
+      include: ['Ripple', 'Tooltip']
+    }
+  },
+  css: ['primevue/resources/themes/saga-blue/theme.css', 'primevue/resources/primevue.min.css', 'primeicons/primeicons.css'],
+})
