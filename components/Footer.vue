8313de35a5552440b9cac5e375e52c102cef758f
Norton Almeida <norton.almeida@gmail.com>
2024-03-18 18:56:01 -0300

    Removed footer border. Code cleanup.

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,9 +3,3 @@
     <p>&copy; {{ new Date().getFullYear() }}</p>
   </footer> 
 </template>
-
-<style scoped lang="scss">
-footer {
-  border: 1px solid red;
-}
-</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -7,6 +7,5 @@
 </template>
 
 <script setup lang="ts">
-import Header from "../components/Header.vue"
-import Footer from "../components/Footer.vue"
+
 </script>
