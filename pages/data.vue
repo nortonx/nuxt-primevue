74eeaa71d78d72ce1d65647ea9ea4fa174504f39
Norton Almeida <norton.almeida@gmail.com>
2024-03-29 08:27:19 -0300

    feat: Added search field to filter data and also added types as exteral files for better code maintenance.

--- pages/data.vue
+++ pages/data.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="surface-ground px-4 py-8 md:px-6 lg:px-8">
-    <div class="text-900 font-bold text-6xl mb-4 text-center">Data List</div>
+    <div class="text-900 font-bold text-6xl mb-4 text-center">Data List {{ searchTerm }}</div>
+    <div class="text-700 font-bold text-2xl mb-4 text-center">Filtered data length: {{ filteredData.length }}</div>
     <div class="grid">
-      <!-- <div class="col-12 lg:col-10 col-offset-1">
+      <div class="col-12 lg:col-10 col-offset-1">
         <div class="shadow-2 p-4 h-full flex flex-column surface-card ">
-          <InputText 
-            placeholder="Search by ACT Symbol" 
-            class="w-full" 
-            v-model="search" 
-            ref="search"
-            @change="handleSearch(search)"
+          <InputText
+            placeholder="Search by ACT Symbol"
+            class="w-full"
+            v-model="searchTerm"
+            ref="searchField"
+            variant="filled"
           ></InputText>
         </div>
-      </div> -->
+      </div>
       <div class="col-12 lg:col-10 col-offset-1">
-        <DataTable :value="jsonData" :paginator="true" :rows="10" :rowsPerPageOptions="[5, 10, 20]">
+        <DataTable :value="filteredData" :paginator="true" :rows="10" :rowsPerPageOptions="[5, 10, 20]">
           <Column field="ACT Symbol" header="ACT Symbol" sortable></Column>
           <Column field="NASDAQ Symbol" header="NASDAQ Symbol" sortable></Column>
           <Column field="Company Name" header="Name" sortable></Column>
@@ -26,12 +27,21 @@
 </template>
 
 <script setup lang="ts">
-import jsonData from "@/data/csvjson.json"
+import rawJsonData from "@/data/csvjson.json"
+import { type Ticker } from "@/types/ticker.type"
 
+const searchField = ref(null)
+const searchTerm = ref("")
+const jsonData = ref<Ticker[]>(rawJsonData)
 
-function handleSearch(search: string) {
-  return jsonData.filter((item: any) => {
-    return item["ACT Symbol"].toLowerCase().includes(search.toLowerCase())
+// computed
+const filteredData = computed(() => {
+  if (!searchTerm.value) {
+    return jsonData.value;
+  }
+  return jsonData.value.filter((item: Ticker) => {
+    return item["ACT Symbol"].toLowerCase().includes(searchTerm.value.toLowerCase())
   })
-}
+})
+
 </script>
