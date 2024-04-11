e108e5b595192bf6e9282d5cc18ed2ec3e03d5f7
Norton Almeida <norton.almeida@gmail.com>
2024-04-11 10:20:58 -0300

    feat: fixed form layout.

--- pages/form.vue
+++ pages/form.vue
@@ -1,34 +1,50 @@
 <template>
   <div class="px-4 py-8 md:px-6 lg:px-8">
-    <div class="grid">
+    <div class="flex justify-content-center">
       <div class="col-12">
-        <div class="formgrid grid">
-          <div class="field col-12 md:col-6">
-            <label for="firstName">First Name</label>
-            <InputText id="firstName" v-model="formData.firstName" class="m-2 w-auto"/>
+        <div class="surface-card p-4 shadow-2 border-round w-full">
+          <div class="text-center mb-5">
+              <div class="text-900 text-3xl font-medium mb-3">Sign Up</div>
+              <span class="text-600 font-medium line-height-3">(For demo purposes only)</span>
           </div>
-          <div class="field col-12 md:col-6">
-            <label for="lastName">Last Name</label>
-            <InputText id="lastName" v-model="formData.lastName" class="m-2 w-auto"/>
-          </div>
-          <div class="field col-12 md:col-6">
-            <label for="email">Email</label>
-            <InputText id="email" v-model="formData.email" class="m-2 w-auto"/>
-          </div>
-          <div class="field col-12 md:col-6">
-            <label for="phone">Phone</label>
-            <InputText id="phone" v-model="formData.phone" class="m-2 w-auto"/>
-          </div>
-          <div class="field col-12 md:col-3">
-            <Dropdown
-              v-model="formData.country"
-              :options="countries"
-              optionLabel="name"
-              placeholder="Select a Country"
-              class="m-2 w-auto"
-            />
+          <div class="formgrid grid">
+            <div class="field col-12 md:col-6">
+              <label for="firstName" class="block text-900 font-medium mb-2">First Name</label>
+              <InputText id="firstName" v-model="formData.firstName" class="w-full mb-3"/>
+            </div>
+            <div class="field col-12 md:col-6">
+              <label for="lastName" class="block text-900 font-medium mb-2">Last Name</label>
+              <InputText id="lastName" v-model="formData.lastName" class="w-full mb-3"/>
+            </div>
+            <div class="field col-12 md:col-6">
+              <label for="email" class="block text-900 font-medium mb-2">Email</label>
+              <InputText id="email" v-model="formData.email" class="w-full mb-3"/>
+            </div>
+            <div class="field col-12 md:col-6">
+              <label for="phone" class="block text-900 font-medium mb-2">Phone</label>
+              <InputText id="phone" v-model="formData.phone" class="w-full mb-3"/>
+            </div>
+            <div class="field col-12 md:col-6">
+              <label for="password" class="block text-900 font-medium mb-2">Password</label>
+              <InputText id="password" type="password" class="w-full mb-3" />
+            </div>
+            <div class="field col-12 md:col-6">
+              <label class="block text-900 font-medium mb-2" for="country">Country</label>
+              <Dropdown
+                id="country"
+                v-model="formData.country"
+                :options="countries"
+                optionLabel="name"
+                placeholder="Select a Country"
+                class="w-full"
+              />
+            </div>
+            <div class="field col-12 action-row flex justify-content-center">
+              <Button label="Sign Up"></Button>
+            </div>
           </div>
         </div>
+        
       </div>
     </div>
   </div>
@@ -39,11 +55,11 @@ import { ref, onMounted, watchEffect } from "vue"
 import Dropdown from "primevue/dropdown"
 import { type FormData } from "@/types/formData.type"
 
-
 const formData = ref<FormData>({
   firstName: "",
   lastName: "",
   email: "",
+  password: "",
   phone: "",
   country: ""
 })
