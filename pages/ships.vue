02fd4ecb89ceee96ef5f7a29b2920df188bee712
Norton Almeida <norton.almeida@gmail.com>
2024-05-25 13:25:34 -0300

    feat: first fetch worked. need to implement layout.

--- pages/ships.vue
+++ pages/ships.vue
@@ -1,43 +1,39 @@
 <template>
   <div class="surface-section px-4 py-8 md:px-6 lg:px-8">
     <div class="text-700 text-center">
-      <div class="text-blue-600 font-bold mb-3"><i class="pi pi-prime"></i>&nbsp;POWERED BY PrimeVue</div>
-      <div class="text-900 font-bold text-5xl mb-3">Push the button for SW Ships</div>
+      <div class="text-blue-600 font-bold mb-3"><i class="pi pi-prime"></i>&nbsp;List of Star Wars Ships:</div>
+      
 
-      <OrderList v-if="data.value" :value="data.value.results" header="Starships" style="height: 300px" :listStyle="{height: '200px'}">
-        <template #header>List of Starships</template>L
-        <template #item="slotProps">
-          <div class="flex flex-wrap p-2 align-items-center gap-3">
-            <img 
-              class="w-4rem flex-shrink-0 border-round"
-              src=""
-              :alt="slotProps.item.name"
-              />
-              <div class="flex-1 flex flex-column gap-2">
-                <span class="font-bold">{{ slotProps.item.name }}</span>
-                <div class="flex align-items-center gap-2">
-                    <i class="pi pi-tag text-sm"></i>
-                    <span>length: {{ slotProps.item.length }}</span>
-                </div>
-            </div>
-            <span class="font-bold">$ {{ slotProps.item.cost_in_credits }}</span>
-          </div>
-        </template>
-      </OrderList>
+      <ProgressSpinner v-if="pending" />
+      <ul v-if="data">
+        <div class="">I really wanted to show the ships image</div>
+        <li v-for="ship in data.results" :key="ship.name">
+          {{ ship.name }} | crew: {{ ship.crew }}
+        </li>
+      </ul>
+      <div v-else>
+        Deu merda
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import OrderList from "primevue/orderlist"
+import ProgressSpinner from "primevue/progressspinner"
 import { onMounted } from "vue"
 
 const { data, pending, error, refresh } = await useFetch("https://swapi.dev/api/starships/", {
-  pick: ["results"]
+  pick: ["results"],
 });
 
 onMounted(() => {
-  console.log("data content:", data.value.results)
+  console.log("data content:", (data as any).value.results)
 })
 
 </script>
+
+<style scoped lang="scss">
+ul {
+  list-style-type: none;
+}
+</style>
