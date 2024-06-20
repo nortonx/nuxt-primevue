96c69c8c146d69b4166c1790552e98a0f3a138d7
Norton Almeida Junior <norton.junior@defensoria.mg.def.br>
2024-06-20 14:26:02 -0300

    cleaned up unit test file.

--- test/components/Car.test.ts
+++ test/components/Car.test.ts
@@ -7,20 +7,18 @@ describe('Car component', () => {
 
   beforeEach(() => {
     wrapper = shallowMount(Car, { props: { carModel: 'Toyota', carPlate: 'ABC-1234', carColor: "black", carOwner: "John Doe" } })
-    // wrapper.setProps({ car: { carModel: 'Toyota', carPlate: 'ABC-1234', carColor: "black", carOwner: { name: "John Doe" } } })
   })
 
   it('should render correctly and match snapshot', () => {
     expect(wrapper.html()).toMatchSnapshot();
   })
 
-  it('should render correctly with props', () => {
-    console.log("DEBUG======================================================>", wrapper.props().carModel)
-
+  it('should render correctly with props and match snapshot', () => {
     expect(wrapper.find('.car-model').text()).toBe(`Model: ${wrapper.props().carModel}`)
     expect(wrapper.find('.car-plate').text()).toBe(`Plate: ${wrapper.props().carPlate}`)
     expect(wrapper.find('.car-color').text()).toBe(`Color: ${wrapper.props().carColor}`)
     expect(wrapper.find('.car-owner').text()).toBe(`Owner: ${wrapper.props().carOwner}`)
+
     expect(wrapper.html()).toMatchSnapshot();
   })
 })
